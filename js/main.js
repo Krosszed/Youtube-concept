1f0bd5aa25e79bf16379a768d88464499c7bc81b
Stanislav <82042656+Krosszed@users.noreply.github.com>
2021-04-09 00:41:31 +0300

    Пофиксили меню и шапку, добавили контент

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper('.channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView:6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+});
+const swiper2 = new Swiper('.channel2-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView:3,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel2-button-next',
+    prevEl: '.channel2-button-prev',
+  },
+});
+const swiper3 = new Swiper('.channel3-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView:6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel3-button-next',
+    prevEl: '.channel3-button-prev',
+  },
+});
+
+
+
